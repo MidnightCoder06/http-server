39825b65bb2c4afb0e04935cde0d4d8d49601ed4
Jean Leconte II <jeanemileleconteii@gmail.com>
2022-01-01 22:09:09 -0600

    built out route class

--- src/router/route.py
+++ src/router/route.py
@@ -2,16 +2,19 @@ import uuid
 
 class Route:
     def __init__(self, req):
-        #request_method, path, parameters = initialize_route(req)
-        u = uuid.uuid1()
-        print(u)
+        self.route = self.initialize_route(req)
 
-    # def initialize_route(req):
-    #     # parsing logic
-    #     request_method =
-    #     path =
-    #     parameters =
-
+    def initialize_route(self, req):
+        route = {}
+        generated_object_id = int(uuid.uuid1())
+        route_properties = req.split('/')
+        route['generated_object_id'] = generated_object_id
+        route['method_name'] = route_properties[0]
+        route['repository_path'] = route_properties[2]
+        if len(route_properties) > 3:
+            route['given_object_id'] = route_properties[3]
+        return route
 
 if __name__ == '__main__':
-    r = Route('fake req')
+    r = Route('PUT /data/{repository}')
+    print(r.route) # {'generated_object_id': 210591014432009452074551886180100096483, 'method_name': 'PUT ', 'repository_path': '{repository}'}
